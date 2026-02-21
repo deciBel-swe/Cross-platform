70959bf6687a1b95a98b41b1c189cd23faaca74d
Abdalrahman Essam <164944475+AbdoCrow@users.noreply.github.com>
2026-02-21 22:23:21 +0200

    feat(UI): Added new app icon image for the application

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:decibel/main.dart';
+import 'package:decibel/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
