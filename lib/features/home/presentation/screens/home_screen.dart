090c62df3e7e33010a97997fb2c841c20fd0cb87
Maths-Error <abdelrahman.salaheng@gmail.com>
2026-03-12 21:31:46 +0200

    test: remove default widget smoke test

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
 
+import '../../../library_profile/models/public_profile_social_links.dart';
+import '../../../library_profile/presentation/widgets/social_links_widget.dart';
+
 /// Empty Home page – placeholder.
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Home', style: TextStyle(fontSize: 24))),
+    return Scaffold(
+      body: Center(
+        child: SocialLinksWidget(
+          socialLinks: const PublicProfileSocialLinks(
+            instagram: 'https://instagram.com/flutter',
+            twitter: 'https://x.com/flutterdev',
+            website: 'https://flutter.dev',
+          ),
+        ),
+      ),
     );
   }
 }
