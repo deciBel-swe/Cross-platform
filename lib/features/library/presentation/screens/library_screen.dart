51ad6471e12fa15290c3662426ef9381ac5702c8
karim tarek <karimtarek2762005@gmail.com>
2026-03-17 21:23:06 +0200

    feat(library/presentation): add track audio waveforms notifier and providers

--- lib/features/library/presentation/screens/library_screen.dart
+++ lib/features/library/presentation/screens/library_screen.dart
@@ -6,7 +6,7 @@ class LibraryScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(child: Text('Library', style: TextStyle(fontSize: 24))),
     );
   }
