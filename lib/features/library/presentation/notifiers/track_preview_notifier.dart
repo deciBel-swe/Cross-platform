27b5bc94a6654829803d37031c4049d953e7b731
Ziadabdelraouf <ziadabdelraouff@gmail.com>
2026-03-28 18:24:00 +0200

    fix(settings):fix flutter analyze issues

--- lib/features/library/presentation/notifiers/track_preview_notifier.dart
+++ lib/features/library/presentation/notifiers/track_preview_notifier.dart
@@ -1,5 +1,5 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../library_profile/presentation/providers/track_preview_provider.dart';
 import '../../../library_profile/presentation/providers/track_repository_provider.dart';
