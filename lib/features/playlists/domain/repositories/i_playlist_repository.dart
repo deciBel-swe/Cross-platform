e8be622a56cf6f028efb082892b632753ad0b55c
Ahmed Mohammed Ahmed AbdAl-Jaleel <76847188+Jel-Jel05@users.noreply.github.com>
2026-03-29 21:48:22 +0200

    refactor(playlist): Return Either from IPlaylistRepository methods

--- lib/features/playlists/domain/repositories/i_playlist_repository.dart
+++ lib/features/playlists/domain/repositories/i_playlist_repository.dart
@@ -1,13 +1,18 @@
+import 'package:dartz/dartz.dart';
+import '../../../../core/errors/failures.dart';
 import '../entities/playlist.dart';
 import '../entities/playlist_metadata.dart';
 
 abstract class IPlaylistRepository {
   /// Creates a new playlist using the provided metadata.
-  Future<Playlist> createPlaylist(PlaylistMetadata metadata);
+  Future<Either<Failure, Playlist>> createPlaylist(PlaylistMetadata metadata);
 
   /// Updates an existing playlist's metadata.
-  Future<Playlist> updatePlaylist(int playlistId, PlaylistMetadata metadata);
+  Future<Either<Failure, Playlist>> updatePlaylist(
+    int playlistId,
+    PlaylistMetadata metadata,
+  );
 
   /// Deletes a playlist by its ID.
-  Future<void> deletePlaylist(int playlistId);
+  Future<Either<Failure, void>> deletePlaylist(int playlistId);
 }
