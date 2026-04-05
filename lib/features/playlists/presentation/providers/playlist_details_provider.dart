bb894a948bb4d19df2429897e5decb4658c6ce22
Ahmed Mohammed Ahmed AbdAl-Jaleel <76847188+Jel-Jel05@users.noreply.github.com>
2026-04-05 14:08:37 +0200

    refactor(playlist): Refactor playlist providers into notifiers

--- lib/features/playlists/presentation/providers/playlist_details_provider.dart
+++ lib/features/playlists/presentation/providers/playlist_details_provider.dart
@@ -1,36 +1,13 @@
-import 'package:dartz/dartz.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../../../core/errors/failures.dart';
 import '../../domain/entities/playlist.dart';
-import 'user_playlists_provider.dart';
+import '../notifiers/playlist_details_notifier.dart';
 
 /// A StateProvider to hold the temporary dragged order before hitting Save
 final pendingTracksProvider = StateProvider.autoDispose<List<int>?>(
   (ref) => null,
 );
 
-class PlaylistDetailsNotifier
-    extends AutoDisposeFamilyAsyncNotifier<Playlist, int> {
-  @override
-  Future<Playlist> build(int arg) async {
-    final repository = ref.watch(playlistRepositoryProvider);
-
-    final result = await repository.getPlaylistDetails(arg);
-
-    return result.fold(
-      (failure) => throw Exception(failure.message),
-      (playlist) => playlist,
-    );
-  }
-
-  Future<Either<Failure, String>> fetchSecretLink() async {
-    final repository = ref.read(playlistRepositoryProvider);
-
-    return await repository.getPlaylistSecretLink(arg);
-  }
-}
-
 final playlistDetailsProvider = AsyncNotifierProvider.autoDispose
     .family<PlaylistDetailsNotifier, Playlist, int>(
-      PlaylistDetailsNotifier.new,
-    );
+  PlaylistDetailsNotifier.new,
+);
