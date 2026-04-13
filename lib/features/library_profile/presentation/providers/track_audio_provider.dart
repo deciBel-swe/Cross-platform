a79fe622f52b2f7f11f30a9a468fee9cab9bb683
Ziadabdelraouf <ziadabdelraouff@gmail.com>
2026-04-13 13:25:02 +0200

    feat(track): add track detail bottom sheet

--- lib/features/library_profile/presentation/providers/track_audio_provider.dart
+++ lib/features/library_profile/presentation/providers/track_audio_provider.dart
@@ -7,3 +7,8 @@ final trackAudioProvider =
     NotifierProvider<TrackAudioNotifier, TrackAudioState>(
       TrackAudioNotifier.new,
     );
+
+/// Set to `false` while any bottom sheet that would overlap the mini player is
+/// open. The [MobileMiniPlayer] in [_MobileShell] respects this flag so it
+/// slides out of sight until the sheet is dismissed.
+final miniPlayerVisibleProvider = StateProvider<bool>((ref) => true);
--- lib/features/library_profile/presentation/screens/web_profiles.dart
+++ lib/features/library_profile/presentation/screens/web_profiles.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import '../../../../core/utils/auth_validators.dart';
 import '../../../../core/theme/app_colors.dart';
+import '../../../../core/utils/auth_validators.dart';
 import '../providers/web_profiles_order_provider.dart';
 import '../providers/web_profiles_provider.dart';
 import '../utils/web_profile_platform_utils.dart';
